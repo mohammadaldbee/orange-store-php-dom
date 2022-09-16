3ed97d03d2da1a0de8c05a78d2b6bf5be0aec73c
mohammadaldbee <96492145+mohammadaldbee@users.noreply.github.com>
2022-09-16 17:22:20 +0300

    php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $phones = [
     [ 
      'name' => 'INFINIX Zero 8',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001278_infinix-zero-8_220.jpeg',
-    'rate' => '0',
+    'rate' => '2',
     'brand' => 'Infinix',
     'price' => 'JOD 205.00',
     'is_out_of_stock' => '1'
@@ -19,7 +19,7 @@ $phones = [
     [ 
      'name' => 'Apple iPhone 12 Pro',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001495_apple-iphone-12-pro_220.jpeg',
-    'rate' => '0',
+    'rate' => '1',
     'brand' => 'Apple',
     'price' => 'JOD 973.00',
     'is_out_of_stock' => '1'
@@ -28,7 +28,7 @@ $phones = [
     [ 
      'name' => 'ITEL A48',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001495_apple-iphone-12-pro_220.jpeg',
-    'rate' => '0',
+    'rate' => '4',
     'brand' => 'iTel',
     'price' => 'JOD 66.00'
     ],
@@ -36,7 +36,7 @@ $phones = [
      'name' => 'Samsung Galaxy S21 Ultra',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001485_samsung-galaxy-s21-ultra_220.jpeg',
 
-    'rate' => '0',
+    'rate' => '3',
     'brand' => 'Samsung',
     'price' => 'JOD 799.00'
     ],
@@ -60,8 +60,14 @@ $phones = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mobile Phone | Orange Jordan E shop</title>
     <!-- Copyright © 2014 Monotype Imaging Inc. All rights reserved -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/orange-helvetica.min.css" rel="stylesheet" integrity="sha384-ARRzqgHDBP0PQzxQoJtvyNn7Q8QQYr0XT+RXUFEPkQqkTB6gi43ZiL035dKWdkZe" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/boosted.min.css" rel="stylesheet" integrity="sha384-Di/KMIVcO9Z2MJO3EsrZebWTNrgJTrzEDwAplhM5XnCFQ1aDhRNWrp6CWvVcn00c" crossorigin="anonymous">
+<style>
+.checked {
+  color: orange;
+}
+</style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -101,7 +107,49 @@ $phones = [
       </form>
     </div>
   </div>
-</nav>
+</nav> <br>
+<br>
+<div class="container">
+  <div class="row row-col-2 row-cols-md-3 g-4">
+  <?php
+  for($i=0;$i<count($phones);$i++){
+    ?>
+  
+  <div class="col">
+    <div class="card" style="width: 18rem;height:auto; margin:auto ">
+  <img src="<?php echo $phones[$i]["img_url"];?>" class="card-img-top"style="width: 9rem;height:200px;margin:auto;margin-top:10px">
+  <div class="card-body">
+    <p class="card-title"><?php echo $phones[$i]['brand'];?></p>
+    <h6 class="card-title"><?php echo $phones[$i]['name'];?></h6>
+    <p class="card-title">
+<?php for ($x=0;$x<5;$x++){
+  if($x<$phones[$i]['rate']){
+   echo '<span class="fa fa-star checked"></span>';
+  } else{
+    echo '<span class="fa fa-star"></span>';
+  }
+  
+
+}?></p>
+    <a href="#"><h6>View Details</h6></a>
+    <hr>
+    <p class="card-text" style="color:orange;"><b><?php echo $phones[$i]['price'];?></b></p>
+    <div class="container">
+    <div class="row" >
+      <div class="col-8">
+    <a href="#" class="btn btn-dark"><h3 style="text-align:center;padding-top:10px; ">Buy Now</h3></a></div>
+    <div class="col-2">
+    <a href="#" class="btn"><img src="https://th.bing.com/th/id/OIP.W1kmWwrs2bJCSbFBmDS4NgHaHa?w=177&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7" style="width: 3rem;height:50px;"></a></div>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
+<?php
+}
+?>
+</div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/js/boosted.bundle.min.js" integrity="sha384-5thbp4uNEqKgkl5m+rMBhqR+ZCs+3iAaLIghPWAgOv0VKvzGlYKR408MMbmCjmZF" crossorigin="anonymous"></script>
 </body>
 </html>
